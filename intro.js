fa95212cf56ea6a43ffbd4327a34ea7535361dcf
jnjerin <44367535+jnjerin@users.noreply.github.com>
2022-06-24 15:41:23 +0300

    added new file; intro,js

---
+++ intro.js
@@ -0,0 +1,7 @@
+//code that prints out your name, height, and country on the screen
+
+const myName = "Joan Njeri"
+const height = " 5'6"
+const country = "Kenya"
+
+console.log("My name is " + myName +". I am " + height+". I live in "+country+"." )
